77049984a800f78b8ab6bdb2e89f46f862283a31
reckbo <reckbo@bwh.harvard.edu>
2014-08-25 06:51:12 -0400

    ENH: Major overhaul - made portable

--- weekly.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-/projects/schiz/log/logusers
-/projects/schiz/log/logschiz
-/projects/schiz/log/logcollab
-/projects/schiz/software/scripts/utils/hosts/hostlist users
-redo /projects/schiz/logplots/schiz.png
-redo /projects/schiz/logplots/users.png
