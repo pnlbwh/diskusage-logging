57c9662ae3371f08fea84947bf980da14c9cb833
reckbo <reckbo@bwh.harvard.edu>
2014-11-11 19:06:18 -0500

    -remove schiz specific scripts

--- logschiz.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-/projects/schiz/log/logdirsizes /projects/schiz/
-/projects/schiz/log/logdirsizes /projects/schiz 3
-/projects/schiz/log/logdirsizes /projects/schiz/guest
-/projects/schiz/log/logdirsizes /projects/schiz/pi
-/projects/schiz/log/logdirsizes /projects/schiz/ra
-/projects/schiz/log/logdirsizes /terastations/A/pnl/Collaborators
-
