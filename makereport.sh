02c3f54a18f7ffabcf033632092f7df6b3ce5d27
reckbo <reckbo@bwh.harvard.edu>
2017-01-19 12:24:29 +0000

    ENH: Add absolute disk usage

--- makereport.sh
+++ makereport.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -eu
-# script=$(readlink -f $(type -p "$0"))
-#dir=$(dirname $script)
-indir=report-lib/
-outdir=_data/
+SCRIPT=$(readlink -m $(type -p $0))
+SCRIPTDIR=$(dirname $SCRIPT)
+indir=$SCRIPTDIR/report-lib/
+outdir=$SCRIPTDIR/_data/
 Rscript -e "library(knitr); knit2html('$indir/report.Rmd', output=\"$outdir/report.html\")"
 
 rm report.md
