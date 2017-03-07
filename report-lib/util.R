2299e0c4183517d24a6aba284443bca8bc8ef85a
reckbo <reckbo@bwh.harvard.edu>
2017-03-07 11:34:46 +0000

    STY: cleanup

--- report-lib/util.R
+++ report-lib/util.R
@@ -1,11 +1,4 @@
-## suppressWarnings(library(data.table, quietly=TRUE))
-## suppressWarnings(library(ggplot2, quietly=TRUE))
-## suppressWarnings(library(magrittr, quietly=TRUE))
-## suppressWarnings(library(lubridate, quietly=TRUE))
-## suppressWarnings(library(xtable, quietly=TRUE))
-
 pkgs <-c('ggplot2', 'data.table', 'xtable', 'magrittr', 'lubridate', 'xtable')
-
 for(p in pkgs) suppressPackageStartupMessages( stopifnot(
                                                library(p, quietly=TRUE,
                                                         logical.return=TRUE,
@@ -18,7 +11,6 @@ printTable <- function(d) {
   }
 }
 
-
 stripHost <- function(hostpath) {
   if (grepl(':', hostpath)) {
       unlist(strsplit(rootDir, ':'))[2]
@@ -26,17 +18,3 @@ stripHost <- function(hostpath) {
     hostpath
   }
 }
-
-changes <- function(dt, k) {
-
-  }
-
-##   %>%
-##         filter(!grepl("Trash", Directory)) %>%
-##         group_by(Directory) %>%
-##         mutate(Datetime=as.character(Datetime)) %>%
-##         mutate(change=(as.numeric(Size) - lag(as.numeric(Size), k))/1024/1024/1024,
-##                 Datetime.before=lag(Datetime, k)) %>%
-##         ungroup() %>%
-##         filter(Datetime==max(df$Datetime)) %>%
-##         arrange(desc(change))
