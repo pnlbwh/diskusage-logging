ac1e37a22ac8af2e35799e62869bbaca581b7771
Isaiah Norton <ihnorton@users.noreply.github.com>
2018-09-28 17:11:28 -0400

    Change ylim for df report, and some debugging output

--- makereport.sh
+++ makereport.sh
@@ -4,7 +4,7 @@ SCRIPT=$(readlink -m $(type -p $0))
 SCRIPTDIR=$(dirname $SCRIPT)
 indir=$SCRIPTDIR/report-lib/
 outdir=$SCRIPTDIR/_data/
-Rscript -e "library(knitr); knit2html('$indir/report.Rmd', output=\"$outdir/report.html\")"
+Rscript -e "library(knitr); knit2html('$indir/report.Rmd', output=\"$outdir/report.html\"); warnings()"
 
-rm report.md
-rm -r figure
+#rm report.md
+#rm -r figure
