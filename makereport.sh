562acc49a7f4a21459f62dcbf8aafe13d436bef7
Billah, Tashrif <tbillah@bwh.harvard.edu>
2020-04-06 10:24:18 -0400

    store reports in htmlreport directory, named after dates

--- makereport.sh
+++ makereport.sh
@@ -3,11 +3,11 @@ set -eu
 SCRIPT=$(readlink -m $(type -p $0))
 SCRIPTDIR=$(dirname $SCRIPT)
 indir=$SCRIPTDIR/report-lib/
-outdir=$SCRIPTDIR/_data/
+outdir=$SCRIPTDIR/_data/htmlreport
+datestamp=$(date +"%Y%m%d")
 
 # change working directory to avoid permission denied errors
 cd $SCRIPTDIR
-Rscript -e "library(knitr); knit2html('$indir/report.Rmd', output=\"$outdir/report.html\"); warnings()"
+Rscript -e "library(knitr); knit2html('$indir/report.Rmd', output=\"$outdir/report-${datestamp}.html\"); warnings()"
 
-#rm report.md
-#rm -r figure
+
