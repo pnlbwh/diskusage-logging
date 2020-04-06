fcd375ce65cbf4612eb4f5c5813d3833d2154333
Billah, Tashrif <tbillah@bwh.harvard.edu>
2020-04-06 10:24:52 -0400

    change DATE to datestamp for consistency

--- mailreport.sh
+++ mailreport.sh
@@ -3,19 +3,21 @@ SCRIPTDIR=$( cd $(dirname $0) ; pwd -P )
 
 # copy the report to a temp directory to avoid permission denied errors
 tmpdir=$(mktemp -d)
-cp $SCRIPTDIR/_data/report.html $tmpdir/report.html
 
 # E.g.
-#  mailreport.sh reckbo sylvain
+# mailreport.sh reckbo sylvain
 
-DATE=$(date +"%Y%m%d")
+datestamp=$(date +"%Y%m%d")
+cp $SCRIPTDIR/_data/htmlreport/report-${datestamp}.html $tmpdir/
 
 from=tbillah@bwh.harvard.edu
-for user in $@; do
-    echo "" | mailx -r $from -s "PNL disk usage report: $DATE  " \
-        -a $tmpdir/report.html \
+for user in $@
+do
+    echo "" | mailx -r $from -s "PNL disk usage report: $datestamp  " \
+        -a $tmpdir/*.html \
         -- $user@bwh.harvard.edu
 done
 
-rm $tmpdir/report.html
-rmdir $tmpdir
+rm -r $tmpdir
+
+
