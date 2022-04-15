edb8a9f8c0e205d5a065b57d638d3894a3d56116
Billah, Tashrif <tbillah@bwh.harvard.edu>
2022-04-15 11:39:15 -0400

    obtain user,name from /etc/passwd

---
+++ manual_finger.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+# finger command is hard to work with via subprocess
+# so we generate the list of users as:
+# grep 12345 /etc/passwd > people.txt
+
+import pandas as pd
+
+# df= pd.DataFrame(columns=['user','fname','uid','gid'])
+df= pd.DataFrame(columns=['user','fname','uid'])
+
+with open('people.txt') as f:
+    lines= f.read().strip().split('\n')
+
+for i,line in enumerate(lines):
+    parts= line.split(":")
+    # df.loc[i]= [parts[1],parts[5].split(',')[0],parts[3],parts[4]]
+    df.loc[i]= [parts[1],parts[5].split(',')[0],parts[3]]
+
+df.sort_values(by='user', inplace=True)    
+
+df.to_csv('user_name.csv', index=False)
