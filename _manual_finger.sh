0b732f7b67e78db973faba51576edd8025547f56
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2024-03-20 13:14:29 -0400

    * use load balancer URL eristwo * use -a with rsync

--- _manual_finger.sh
+++ _manual_finger.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-ssh -o StrictHostKeyChecking=no eris2n4.research.partners.org "/data/predict1/diskusage-logging/manual_finger.sh"
+ssh -o StrictHostKeyChecking=no eristwo.partners.org "/data/predict1/diskusage-logging/manual_finger.sh"
 
 cd /data/predict1/diskusage-logging/
 
 /data/predict1/miniconda3/bin/python manual_finger.py
 
-rsync user_name.csv pnl-x80-1.partners.org:/rfanfs/pnl-zorro/software/diskusage-logging/
+rsync -a user_name.csv pnl-x80-1.partners.org:/rfanfs/pnl-zorro/software/diskusage-logging/
 
